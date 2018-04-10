3caba2ee47c240346a18b17b02b76c734b3f1c90
Georgy Marrero <georgymarrero@berkeley.edu>
2018-04-10 10:34:27 -0700

    Fix evaluate.py and put dataset sizes inside params

--- evaluate.py
+++ evaluate.py
@@ -5,7 +5,7 @@ import os
 
 import tensorflow as tf
 
-from model.input_fn import input_fn
+from model.input_fn import test_input_fn
 from model.model_fn import model_fn
 from model.utils import Params
 
@@ -27,22 +27,12 @@ if __name__ == '__main__':
     assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
     params = Params(json_path)
 
-    # Create the input data pipeline
-    tf.logging.info("Creating the datasets...")
-    data = tf.contrib.learn.datasets.mnist.load_mnist(args.data_dir)
-
-    # Specify the sizes of the dataset we evaluate on
-    params.eval_size = data.test.num_examples
-
-    # Create the test input function
-    test_input_fn = lambda: input_fn(False, data.test.images, data.test.labels, params)
-
     # Define the model
     tf.logging.info("Creating the model...")
     estimator = tf.estimator.Estimator(model_fn, params=params, model_dir=args.model_dir)
 
     # Evaluate the model on the test set
     tf.logging.info("Evaluation on the test set.")
-    res = estimator.evaluate(test_input_fn)
+    res = estimator.evaluate(lambda: test_input_fn(args.data_dir, params))
     for key in res:
         print("{}: {}".format(key, res[key]))
