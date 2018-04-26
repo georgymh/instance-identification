539bb197be11820818ca65cd08fec9d24c36ae9e
Georgy Marrero <georgymarrero@berkeley.edu>
2018-04-26 15:19:37 -0700

    Allow starting from pre-trained models

--- train.py
+++ train.py
@@ -31,18 +31,22 @@ if __name__ == '__main__':
     config = tf.estimator.RunConfig(tf_random_seed=230,
                                     model_dir=args.model_dir,
                                     save_summary_steps=params.save_summary_steps,
-                                    save_checkpoints_steps=1000)
-    estimator = tf.estimator.Estimator(model_fn, params=params, config=config)
+                                    save_checkpoints_steps=params.save_checkpoints_steps)
+    ws = tf.estimator.WarmStartSettings(
+        ckpt_to_initialize_from=params.warm_start_from,
+        vars_to_warm_start='.*',
+    )
+    estimator = tf.estimator.Estimator(model_fn, params=params, config=config,
+                                       warm_start_from=ws)
 
     # Define the dataset
     tf.logging.info("Defining the dataset...")
+    assert params.dataset in ['mnist', 'imagenetvid'], \
+        "Dataset {} not supported".format(params.dataset)
     if params.dataset == 'mnist':
         train_input_fn = lambda: mnist_train_input_fn('data/mnist', params)
     elif params.dataset == 'imagenetvid':
         train_input_fn = lambda: imagenet_train_input_fn(params)
-    else:
-        # Error -- dataset should be defined
-        exit(1)
 
     # Train the model
     tf.logging.info("Starting training for {} epoch(s).".format(params.num_epochs))
